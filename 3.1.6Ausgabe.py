92d5c8ca42a84b56293e5fc9d417d72bf4e4d8a2
Gitea <test@test.test>
2025-05-23 11:48:01 +0200

    Aufgabe 3.1.6 Ausgabe

---
+++ 3.1.6Ausgabe.py
@@ -0,0 +1,26 @@
+Name = "Max Mustermann"
+Alter = "30"
+Geschlecht = "männlich"
+Größe = "1,80m"
+Gewicht = "75kg"
+Augenfarbe = "braun"
+Haarfarbe = "schwarz"
+Beruf = "Softwareentwickler"
+Hobby = "Lesen"
+Charaktereigenschaften = "freundlich"
+
+print(Name, "ist", Alter, "und", Geschlecht + ".", "Seine Größe ist", Größe, "und er wiegt", Gewicht + ".", "Sein Haar ist", Haarfarbe, "und er hat", Augenfarbe + "e", "Augen.", "Außerdem ist er", Charaktereigenschaften, "und", Hobby, "ist sein Hobby.", "Von Beruf ist er", Beruf + ".")
+
+
+Name = "Erik Berger"
+Alter = "45"
+Geschlecht = "männlich"
+Größe = "1,76m"
+Gewicht = "78kg"
+Augenfarbe = "blau"
+Haarfarbe = "blond"
+Beruf = "IT Support"
+Hobby = "Fitness"
+Charaktereigenschaften = "gelassen"
+
+print(Name, "ist", Alter, "und", Geschlecht + ".", "Seine Größe ist", Größe, "und er wiegt", Gewicht + ".", "Sein Haar ist", Haarfarbe, "und er hat", Augenfarbe + "e", "Augen.", "Außerdem ist er", Charaktereigenschaften, "und", Hobby, "ist sein Hobby.", "Von Beruf ist er", Beruf + ".")
